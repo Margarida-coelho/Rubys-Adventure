25305cb2de6bf4bdc6508c29b1a4f4f60da53ad5
Margarida-coelho <162127564+Margarida-coelho@users.noreply.github.com>
2024-04-04 16:44:00 +0100

    World Interactions - Blocking Movement

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -6,24 +6,34 @@ public class RubyController : MonoBehaviour
 {
     Vector2 position;
 
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
     // Start is called before the first frame update
     void Start()
     {
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
+        horizontal = Input.GetAxis("Horizontal");
 
-        float vertical = Input.GetAxis("Vertical");
+        vertical = Input.GetAxis("Vertical");
 
-        position = transform.position;
+        
+    }
+
+    private void FixedUpdate()
+    {
+        position = rigidbody2d.position;
 
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
 
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
 
-        transform.position = position;
+        rigidbody2d.MovePosition(position);
     }
 }
